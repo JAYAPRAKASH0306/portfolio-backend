4458e1334e1bf7b2179aa06190be8322a36f8f78
JAYAPRAKASH0306 <jayaprakashk0306@gmail.com>
2023-09-18 16:16:44 +0530

    second

--- db/db.js
+++ db/db.js
@@ -1,10 +1,23 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://0.0.0.0:27017/", {
-    dbName:"portfolio",
+// Check if the MONGODB_URI environment variable is set
+if (!process.env.MONGODB_URI) {
+  console.error("MONGODB_URI environment variable is not set.");
+  process.exit(1);
+}
+
+// Connect to MongoDB using the provided URI
+mongoose.connect(process.env.MONGODB_URI, {
+  dbName: "portfolio",
   useNewUrlParser: true,
   useUnifiedTopology: true,
-});
+})
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+  });
 
 const contactSchema = new mongoose.Schema({
   name: String,
