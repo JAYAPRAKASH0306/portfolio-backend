89a1d237878e0cd002f79e38697b171b1f3d6314
JAYAPRAKASH0306 <jayaprakashk0306@gmail.com>
2023-09-19 09:34:36 +0530

    save

--- db/db.js
+++ db/db.js
@@ -1,14 +1,15 @@
 const mongoose = require("mongoose");
+const uri="mongodb+srv://jayaprakash:16p25l0052@jp.djhe0bu.mongodb.net/portfolio"
 
 // Check if the MONGODB_URI environment variable is set
-if (!process.env.MONGODB_URI) {
+if (!uri) {
   console.error("MONGODB_URI environment variable is not set.");
   process.exit(1);
 }
 
 
 // Connect to MongoDB using the provided URI
-mongoose.connect(process.env.MONGODB_URI, {
+mongoose.connect(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
